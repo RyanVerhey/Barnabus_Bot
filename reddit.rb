8debf20f583180947229a90bc784d4dab70aa5b6
RyanVerhey <ryan90v@gmail.com>
2014-09-03 23:47:21 -0400

    Saving both modhash and cookie

--- reddit.rb
+++ reddit.rb
@@ -2,7 +2,9 @@ class YogscastKim
   include HTTParty
 
   def initialize
-    @modhash = YogscastKim.login
+    data = YogscastKim.login
+    @modhash = data[0]
+    @cookie = data[1]
   end
 
   private
@@ -11,8 +13,9 @@ class YogscastKim
     username = 'Barnabus_Bot'
     password = ENV['PASS']
     options = { body: { user: username, passwd: password, api_type: 'json' } }
-    response = HTTParty.post("http://www.reddit.com/api/login/", options)
-    response['json']['data']['modhash']
+    response = YogscastKim.post("http://www.reddit.com/api/login/", options)
+    puts [response['json']['data']['modhash'], response['json']['data']['cookie']]
+    [response['json']['data']['modhash'], response['json']['data']['cookie']]
   end
 
   def to_s
