d04c66387e784e40d594114a92c0b1c9b5a173c0
RyanVerhey <ryan90v@gmail.com>
2014-09-04 22:29:35 -0400

    Method that fetches the latest videos

--- youtube.rb
+++ youtube.rb
@@ -4,6 +4,17 @@ class YouTube
     @client = YouTubeIt::Client.new(:dev_key => ENV['YTKEY'])
   end
 
+  def fetch_videos
+    kim = @client.profile('yogscastkim')
+    activity = @client.activity('yogscastkim')
+    videos = []
+    activity.each do |upload|
+      video = @client.video_by(upload.video_id)
+      videos << Video.new(upload.video_id, video.updated_at, video.title, video.author.uri.split("/").last)
+    end
+    videos
+  end
+
 class Video
   attr_reader :id, :updated_at, :url, :title, :author
 
