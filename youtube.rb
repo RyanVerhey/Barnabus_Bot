0b9ee63f1b280c3c166eb55dac070d68f4d9f9ac
RyanVerhey <ryan90v@gmail.com>
2014-09-04 22:28:06 -0400

    Special Video class to collect all info I need in one place

--- youtube.rb
+++ youtube.rb
@@ -4,4 +4,15 @@ class YouTube
     @client = YouTubeIt::Client.new(:dev_key => ENV['YTKEY'])
   end
 
+class Video
+  attr_reader :id, :updated_at, :url, :title, :author
+
+  def initialize(id, updated_at, title, author)
+    @id = id
+    @updated_at = updated_at
+    @url = "http://www.youtube.com/watch?v=" + @id
+    @title = title
+    @author = author
+  end
+
 end
