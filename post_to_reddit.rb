db52d5179e65ba633ff890e2d04f0a24da240dce
RyanVerhey <ryan90v@gmail.com>
2014-09-07 00:17:38 -0400

    Removed test post

--- post_to_reddit.rb
+++ post_to_reddit.rb
@@ -3,7 +3,6 @@ class PostToReddit
   def self.post(videos, reddit_client)
     video_to_post = videos.first
     if video_to_post
-      # response = response = reddit_client.submit("Test Title 2", "https://www.youtube.com/watch?v=Qpty3oPS55k", "GildedGrizzly")
       response = reddit_client.submit(video_to_post.title, video_to_post.url, "yogscastkim")
       if !response["json"]["errors"].first
         YouTube.save_video_data(video_to_post)
