d9a1cbe1dc421ca50e6580eeb61d6189a72103b3
RyanVerhey <ryan90v@gmail.com>
2014-09-07 00:16:30 -0400

    changed post location to the YogscastKim subreddit

--- post_to_reddit.rb
+++ post_to_reddit.rb
@@ -3,8 +3,8 @@ class PostToReddit
   def self.post(videos, reddit_client)
     video_to_post = videos.first
     if video_to_post
-      response = reddit_client.submit(video_to_post.title, video_to_post.url, "GildedGrizzly")
       # response = response = reddit_client.submit("Test Title 2", "https://www.youtube.com/watch?v=Qpty3oPS55k", "GildedGrizzly")
+      response = reddit_client.submit(video_to_post.title, video_to_post.url, "yogscastkim")
       if !response["json"]["errors"].first
         YouTube.save_video_data(video_to_post)
         puts "Video posted! #{response["json"]["data"]["url"]}"
