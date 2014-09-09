5a11ec6a5b185cd1a04862eae7c931b5b1d68aa5
RyanVerhey <ryan90v@gmail.com>
2014-09-09 16:45:34 -0400

    Took class name change and instance variable changes into account

--- reddit.rb
+++ reddit.rb
@@ -16,7 +16,12 @@ class Reddit
     end
   end
 
-  def submit(title, message, sr, link = true, save = true, resubmit = false)
+  private
+
+  def self.submit(title, message, sr, link = true, save = true, resubmit = false)
+    data = Reddit.login
+    modhash = data[0]
+    cookie = data[1]
     kind = link ? "link" : "self"
     url = link ? message : false
     text = link ? false : message
@@ -29,28 +34,22 @@ class Reddit
       save: save,
       resubmit: resubmit,
       api_type: 'json',
-      uh: @modhash,
+      uh: modhash,
     }, headers: {
       'User-Agent' => 'Barnabus_Bot, proudly built by /u/GildedGrizzly',
-      'X-Modhash' => @modhash,
-      'Cookie' => 'reddit_session=' + @cookie
+      'X-Modhash' => modhash,
+      'Cookie' => 'reddit_session=' + cookie
     } }
-    response = YogscastKim.post('http://www.reddit.com/api/submit', options)
+    response = Reddit.post('http://www.reddit.com/api/submit', options)
   end
-
-  private
 
   def self.login
     username = 'Barnabus_Bot'
     password = ENV['PASS']
     options = { body: { user: username, passwd: password, api_type: 'json' } }
-    response = YogscastKim.post("http://www.reddit.com/api/login/", options)
+    response = Reddit.post("http://www.reddit.com/api/login/", options)
     data = response['json']['data']
     return [data['modhash'], data['cookie']]
-  end
-
-  def to_s
-    @modhash
   end
 
 end
