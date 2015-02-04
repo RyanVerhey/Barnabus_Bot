f877563366b6316a485fec15fc44227a91001da2
RyanVerhey <ryan90v@gmail.com>
2015-02-03 23:46:35 -0500

    Added timestamping to barnabus.rb

--- barnabus.rb
+++ barnabus.rb
@@ -15,15 +15,15 @@ require_relative 'reddit'
 require_relative 'youtube'
 
 input = ARGV
-REDDITNAME = input[1].to_sym
+REDDITNAME = input[1].to_sym if input[1]
 
 case input.first
 when "-r"
   if DATA[:reddits][REDDITNAME]
+    puts "Getting videos for /r/#{REDDITNAME.to_s}. Took #{Time.now - STARTTIME} seconds to initialize..."
     yt = YouTube.new channels: DATA[:reddits][REDDITNAME][:channels]
     yt.recent_videos
     yt.new_videos
-    p yt.new_videos.count
     if yt.new_videos.empty?
       puts "No new videos to post!"
     else
@@ -32,6 +32,7 @@ when "-r"
       end
       ReadWrite.write_recent_vids(recents: yt.recent_videos, reddit: REDDITNAME)
     end
+    end_time = Time.now
   else
     puts "That subreddit hasn't been saved yet."
   end
@@ -40,6 +41,7 @@ when "-u"
     yt.recent_videos
     ReadWrite.write_recent_vids(recents: yt.recent_videos, reddit: REDDITNAME)
     puts "Recent videos for #{REDDITNAME.to_s} successfully updated!"
+    end_time = Time.now
 when "help"
   puts "Hi, I'm Barnabus! Here's a list of my commands:"
   puts "  '-r subreddit':   Searches for new videos and posts them to reddit."
@@ -47,4 +49,8 @@ when "help"
   puts "  'help':           I hope you know what this does :)"
 else
   puts "That command is not recognized. Type 'help' for a list of commands."
+end
+
+if end_time
+  puts "Barnabus took #{(end_time - STARTTIME)} seconds to run."
 end
