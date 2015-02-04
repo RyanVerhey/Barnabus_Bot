34d8a09a5890b564bb0486215bbe8bf10508cde6
RyanVerhey <ryan90v@gmail.com>
2015-02-03 23:28:04 -0500

    Built out barnabus.rb controller

--- barnabus.rb
+++ barnabus.rb
@@ -8,50 +8,43 @@ require 'httparty'
 require 'shenanigans'
 require 'json'
 require 'yaml'
+require_relative 'video'
+require_relative 'read_write'
+DATA = ReadWrite.load_data
 require_relative 'reddit'
 require_relative 'youtube'
-require_relative 'read_write'
 
 input = ARGV
-
-data = ReadWrite.load_data.to_ostruct
+REDDITNAME = input[1].to_sym
 
 case input.first
-when "run"
-  # do everything
-when "update"
-  # Only update the most recent videos
+when "-r"
+  if DATA[:reddits][REDDITNAME]
+    yt = YouTube.new channels: DATA[:reddits][REDDITNAME][:channels]
+    yt.recent_videos
+    yt.new_videos
+    p yt.new_videos.count
+    if yt.new_videos.empty?
+      puts "No new videos to post!"
+    else
+      yt.new_videos.each do |video|
+        Reddit.submit_video video
+      end
+      ReadWrite.write_recent_vids(recents: yt.recent_videos, reddit: REDDITNAME)
+    end
+  else
+    puts "That subreddit hasn't been saved yet."
+  end
+when "-u"
+    yt = YouTube.new channels: DATA[:reddits][REDDITNAME][:channels]
+    yt.recent_videos
+    ReadWrite.write_recent_vids(recents: yt.recent_videos, reddit: REDDITNAME)
+    puts "Recent videos for #{REDDITNAME.to_s} successfully updated!"
 when "help"
   puts "Hi, I'm Barnabus! Here's a list of my commands:"
-  puts "  'run':      Searches for new videos and posts them to reddit."
-  puts "  'update':   Only updates the most recent videos in the database, doesn't post anything to reddit. If it is a new channel/subreddit, initializes the values."
-  puts "  'help':     I hope you know what this does :)"
+  puts "  '-r subreddit':   Searches for new videos and posts them to reddit."
+  puts "  '-u subreddit':   Only updates the most recent videos in the database, doesn't post anything to reddit. If it is a new channel/subreddit, initializes the values."
+  puts "  'help':           I hope you know what this does :)"
 else
   puts "That command is not recognized. Type 'help' for a list of commands."
 end
-
-#
-# client = Google::APIClient.new(
-#   :application_name => 'Barnabus_Bot',
-#   :application_version => '1.0.0'
-# )
-# yt = client.discovered_api('youtube', "v3")
-# client.authorization = nil
-# key = ENV['YTKEY']
-# result = client.execute :key => key, :api_method => yt.playlist_items.list, :parameters => { playlistId: "UUUxoapwoGN9cKN5SPKGVh7A", part: "snippet", maxResults: 10 }
-#p  YAML.load(result.body)#["items"].each { |x| p x }
-# youtube_client = YouTube.get_video_time { YouTube.new }
-
-# if input.first == "save_newest"
-#   youtube_client.save_most_recent_video
-# else
-#   Reddit.submit_video(youtube_client.new_videos)
-# end
-
-# end_time = Time.now
-# puts " "
-# puts "Time Elapsed: " + (end_time - start_time).to_s
-# puts " "
-# puts "Start time: " + start_time.to_s
-# puts "End time: " + end_time.to_s
-
