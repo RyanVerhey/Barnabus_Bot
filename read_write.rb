5797b1fc138c81bf2a0c7d6dd04c033e7fde7ed2
RyanVerhey <ryan90v@gmail.com>
2015-02-03 21:52:25 -0500

    Added ReadWrite.write and ReadWrite.write_recent_vids

--- read_write.rb
+++ read_write.rb
@@ -1,7 +1,21 @@
 class ReadWrite
-  
+
   def self.load_data
     YAML.load(File.read(File.expand_path(File.dirname(__FILE__)) + "/data.yaml"))
   end
-  
+
+  def self.write_recent_vids(params)
+    data = DATA
+    data[:reddits][params[:reddit]][:channels].each do |channel_name,channel_data|
+      channel_data[:recents] = params[:recents][channel_name]
+    end
+    self.write(data)
+  end
+
+  def self.write(data)
+    File.open(File.expand_path(File.dirname(__FILE__)) + "/data.yaml", 'w') do |f|
+      f.write(data.to_yaml)
+    end
+  end
+
 end
