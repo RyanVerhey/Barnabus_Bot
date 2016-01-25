ec207d48cfdc9ce7baefc8cb76d1b26f411f5a69
RyanVerhey <ryan90v@gmail.com>
2016-01-25 18:44:54 -0500

    Refactored the YouTube class a bit

--- youtube.rb
+++ youtube.rb
@@ -1,5 +1,7 @@
 class YouTube
 
+  attr_reader :recent_videos
+
   def initialize(data = {})
     @client = Google::APIClient.new(
       :application_name => 'Barnabus_Bot',
@@ -9,58 +11,67 @@ class YouTube
     @client.authorization = nil
     @key = ENV['YTKEY']
     @channels = data.fetch(:channels, {})
-    @recents = nil
-    @new_vids = nil
+    @recent_videos = recent_vids
+    @new_videos = new_vids if data[:fetch_new]
   end
 
-  def recent_videos(force = false)
-    if !@recents || force
-      if @channels.empty?
-        raise "Can't fetch videos - no channels defined."
+  def new_videos
+    @new_videos ||= new_vids
+  end
+
+  private
+
+  def recent_vids
+    raise "Can't fetch videos - no channels defined." if @channels.empty?
+
+    recents = {}
+    @channels.each do |channel,data|
+      puts "Fetching #{channel.to_s} videos..."
+      channel_list = @client.execute(:key => @key,
+                                     :api_method => @api.channels.list,
+                                     :parameters => { forUsername: channel.to_s, part: "id" })
+      channel_id = YAML.load(channel_list.body)["items"][0]["id"]
+      video_list = @client.execute(:key => @key,
+                                   :api_method => @api.search.list,
+                                   :parameters => {
+                                     channelId: channel_id,
+                                     part: "id,snippet",
+                                     maxResults: 10,
+                                     order: "date",
+                                     type: "video" })
+      videos = YAML.load(video_list.body)["items"]
+
+      sorted_videos = []
+      videos.each do |video|
+        title = video["snippet"]["title"]
+        # desc =
+        if data[:regexp].match(title)
+          sorted_videos <<
+            Video.new(id: video["id"]["videoId"],
+                      published_at: video["snippet"]["publishedAt"],
+                      title: video["snippet"]["title"],
+                      author: video["snippet"]["channelTitle"])
+        end
       end
-      recents = {}
-      @channels.each do |channel,data|
-        puts "Fetching #{channel.to_s} videos..."
-        channel_list = @client.execute :key => @key, :api_method => @api.channels.list, :parameters => { forUsername: channel.to_s, part: "id" }
-        channel_id = YAML.load(channel_list.body)["items"][0]["id"]
-        video_list = @client.execute :key => @key, :api_method => @api.search.list, :parameters => { channelId: channel_id, part: "id,snippet", maxResults: 10, order: "date", type: "video" }
-        videos = YAML.load(video_list.body)["items"]
-        sorted_videos = []
+      recents[channel] = sorted_videos
+    end
+    recents
+  end
+
+  def new_vids
+    new_vids = []
+    if @recent_videos
+      @recent_videos.each do |channel_name,videos|
         videos.each do |video|
-          title = video["snippet"]["title"]
-          if data[:regexp].match(title)
-            sorted_videos << Video.new(id: video["id"]["videoId"],
-                             published_at: video["snippet"]["publishedAt"],
-                             title: video["snippet"]["title"],
-                             author: video["snippet"]["channelTitle"])
+          if YouTube.new?(channel_name.to_s, video.id)
+            new_vids << video
           end
         end
-        recents[channel] = sorted_videos
       end
-      @recents = recents
     else
-      @recents
+      raise "Can't get new videos if you haven't fetched recent videos first!"
     end
-  end
-
-  def new_videos(force = false)
-    if !@new_vids || force
-      new_vids = []
-      if @recents
-        @recents.each do |channel_name,videos|
-          videos.each do |video|
-            if YouTube.new?(channel_name.to_s, video.id)
-              new_vids << video
-            end
-          end
-        end
-      else
-        raise "Can't get new videos if you haven't fetched recent videos first!"
-      end
-      @new_vids = new_vids
-    else
-      @new_vids
-    end
+    new_vids
   end
 
   def update
