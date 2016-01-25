73b7e464594e3f693247a82fa1ddc3446bdf46b8
RyanVerhey <ryan90v@gmail.com>
2016-01-25 18:44:23 -0500

    Moved run and update commands into one case statement to make things a little DRYer

--- barnabus.rb
+++ barnabus.rb
@@ -10,8 +10,6 @@ require_relative 'read_write'
 require_relative 'reddit'
 require_relative 'youtube'
 
-DATA = ReadWrite.load_data
-
 input = ARGV
 REDDITNAME = input[1].to_sym if input[1]
 
@@ -25,33 +23,40 @@ when !ENV["PASS"]
 end
 
 case input.first
-when "-r" # Run
+when "-r", "-u" # Run, Update
+  DATA = ReadWrite.load_data
+
   if DATA[:reddits][REDDITNAME]
     puts "Getting videos for /r/#{REDDITNAME.to_s}..."
     yt = YouTube.new channels: DATA[:reddits][REDDITNAME][:channels]
-    yt.recent_videos
-    yt.new_videos
-    if yt.new_videos.empty?
-      puts "No new videos to post!"
-    else
-      yt.new_videos.each do |video|
-        Reddit.submit_video video
+    binding.pry
+    case input.first
+    when "-r" # Run
+      if yt.new_videos.empty?
+        puts "No new videos to post!"
+      else
+        yt.new_videos.each do |video|
+          Reddit.submit_video video
+        end
+        ReadWrite.write_recent_vids(recents: yt.recent_videos, reddit: REDDITNAME)
       end
+
+    when "-u" # Update
       ReadWrite.write_recent_vids(recents: yt.recent_videos, reddit: REDDITNAME)
+      puts "Recent videos for #{REDDITNAME.to_s} successfully updated!"
+
     end
+
   else
     puts "That subreddit hasn't been saved yet."
   end
-when "-u" # Update
-  yt = YouTube.new channels: DATA[:reddits][REDDITNAME][:channels]
-  yt.recent_videos
-  ReadWrite.write_recent_vids(recents: yt.recent_videos, reddit: REDDITNAME)
-  puts "Recent videos for #{REDDITNAME.to_s} successfully updated!"
+
 when "help" #Help
   puts "Hi, I'm Barnabus! Here's a list of my commands:"
   puts "  '-r subreddit':   Searches for new videos and posts them to reddit."
   puts "  '-u subreddit':   Only updates the most recent videos in the database, doesn't post anything to reddit. If it is a new channel/subreddit, initializes the values."
   puts "  'help':           I hope you know what this does :)"
+
 else
   puts "That command is not recognized. Type 'help' for a list of commands."
 end
