45dd04b8d7028a8b2e1150b0e98bddbe31a65805
RyanVerhey <ryan90v@gmail.com>
2016-05-08 22:41:49 -0400

    Added some required variables to YoutubeChannel

--- app/models/youtube_channel.rb
+++ app/models/youtube_channel.rb
@@ -3,4 +3,12 @@ class YoutubeChannel < ActiveRecord::Base
   has_and_belongs_to_many :subreddits
   serialize :regexp
 
+  CLIENT = Google::APIClient.new(
+    :application_name => 'Barnabus_Bot',
+    :application_version => '3.0.0'
+  )
+  API = CLIENT.discovered_api('youtube', "v3")
+  CLIENT.authorization = nil
+  KEY = ENV['YTKEY']
+
 end
