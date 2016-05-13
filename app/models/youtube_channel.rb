3b0915539749d5267befd12d6195b1120fdfc4b6
RyanVerhey <ryan90v@gmail.com>
2016-05-13 00:30:22 -0400

    Added method to grab recent videos by subreddit

--- app/models/youtube_channel.rb
+++ app/models/youtube_channel.rb
@@ -4,6 +4,11 @@ class YoutubeChannel < ActiveRecord::Base
   has_many :subreddits, through: :channel_assignments
   accepts_nested_attributes_for :channel_assignments
 
+  def recent_videos(subreddit)
+    assignment = channel_assignments.find_by(subreddit: subreddit)
+    videos.where('title REGEXP :regexp OR description REGEXP :regexp', { regexp: assignment.regexp.source }).limit(10)
+  end
+
   def to_s
     self.name
   end
