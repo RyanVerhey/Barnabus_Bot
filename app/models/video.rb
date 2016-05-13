81a4dd77cdd7e762eae338e793043af4f1902d88
RyanVerhey <ryan90v@gmail.com>
2016-05-12 22:05:47 -0400

    Added method that returns videos that don't have an associated channel

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,6 +3,10 @@ class Video < ActiveRecord::Base
 
   has_and_belongs_to_many :channels, class_name: "YoutubeChannel"
 
+  def self.without_associated_channel
+    all.select { |video| video.channels.empty? }
+  end
+
   def self.save_as_video(upload, video)
     Video.new(upload.video_id, video.published_at, video.title, video.author.uri.split("/").last)
   end
