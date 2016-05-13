073042a6ace5dda9384504503ebf1a58316cdceb
RyanVerhey <ryan90v@gmail.com>
2016-05-13 00:29:55 -0400

    Added after_initialize that sets url

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,6 +3,10 @@ class Video < ActiveRecord::Base
 
   has_and_belongs_to_many :channels, class_name: "YoutubeChannel"
 
+  after_initialize do
+    self.url = "https://www.youtube.com/watch?v=" + id
+  end
+
   def self.without_associated_channel
     all.select { |video| video.channels.empty? }
   end
