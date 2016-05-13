d09d026dcb811fc42e759957f0c7414f58e365de
RyanVerhey <ryan90v@gmail.com>
2016-05-12 22:05:05 -0400

    Added channel association to videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,8 @@
 class Video < ActiveRecord::Base
   self.primary_key = "id"
 
+  has_and_belongs_to_many :channels, class_name: "YoutubeChannel"
+
   def self.save_as_video(upload, video)
     Video.new(upload.video_id, video.published_at, video.title, video.author.uri.split("/").last)
   end
