ca23d580bcfaac2ee2bfde748a11e7975f8d18cb
RyanVerhey <ryan90v@gmail.com>
2016-05-13 01:07:15 -0400

    Added method to post new videos to reddit

--- app/controllers/run_controller.rb
+++ app/controllers/run_controller.rb
@@ -7,6 +7,29 @@ class RunController
         channel.videos = new_recents
         channel.save
       end
+      puts "" # To put a line break in between subreddits
+    end
+  end
+
+  def self.post_new_videos(subreddits)
+    subreddits.each do |subreddit|
+      new_vids = {}
+      subreddit.channels.each do |channel|
+        new_vids[channel.to_s] = YouTube.get_new_videos_for_subreddit_and_youtube_channel(subreddit: subreddit, channel: channel)
+      end
+
+      reddit = Reddit.new(subreddit.account)
+      reddit.login
+      new_vids.values.flatten.each do |vid|
+        reddit.submit_video video: vid, subreddit: subreddit
+      end
+
+      subreddit.channels.each do |channel|
+        channel.videos += new_vids[channel.to_s]
+        channel.save
+      end
+
+      puts "" # To put a line break in between subreddits
     end
   end
 end
