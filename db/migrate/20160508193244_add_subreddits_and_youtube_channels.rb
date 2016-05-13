b6fc09eb1f092a8d49d2c29f108630311c6a85f5
RyanVerhey <ryan90v@gmail.com>
2016-05-12 21:02:39 -0400

    Made YouTube channel ID from YouTube the primary key for YoutubeChannels

--- db/migrate/20160508193244_add_subreddits_and_youtube_channels.rb
+++ db/migrate/20160508193244_add_subreddits_and_youtube_channels.rb
@@ -1,22 +1,26 @@
 class AddSubredditsAndYoutubeChannels < ActiveRecord::Migration
   def change
     create_table :subreddits do |t|
-      t.string  :name
+      t.string     :name
       t.belongs_to :reddit_account
 
       t.timestamps null: false
     end
 
-    create_table :youtube_channels do |t|
-      t.string  :name
+    create_table :youtube_channels, id: false do |t|
+      t.string :id
+      t.string :name
+
+      t.index :id, unique: true
 
       t.timestamps null: false
     end
+    execute "ALTER TABLE youtube_channels ADD PRIMARY KEY (id);"
 
     create_table :channel_assignments do |t|
       t.belongs_to :subreddit, index: true
       t.belongs_to :youtube_channel, index: true
-      t.string  :regexp
+      t.string     :regexp
 
       t.timestamps null: false
     end
