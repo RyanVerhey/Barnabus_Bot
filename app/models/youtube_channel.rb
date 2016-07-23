174ac7dc24c0129dea1f3ef880a282ac6ae526f0
RyanVerhey <ryan90v@gmail.com>
2016-07-23 17:46:56 -0400

    Don't need this imperfect method anymore

--- app/models/youtube_channel.rb
+++ app/models/youtube_channel.rb
@@ -4,11 +4,6 @@ class YoutubeChannel < ActiveRecord::Base
   has_many :subreddits, through: :channel_assignments
   accepts_nested_attributes_for :channel_assignments
 
-  def recent_videos(subreddit)
-    regexp = channel_assignments.find_by(subreddit: subreddit).regexp
-    videos.all.order("published_at DESC").limit(10).select { |v| regexp.match(v.title) || regexp.match(v.description) }
-  end
-
   def to_s
     self.name
   end
