d580709c78e633360e2ae87776dcd9d922704853
RyanVerhey <ryan90v@gmail.com>
2016-09-06 20:19:04 -0400

    Forgot about inheritances for class variables

--- app/controllers/barnabus_controller.rb
+++ app/controllers/barnabus_controller.rb
@@ -1,23 +1,21 @@
 class BarnabusController
   class Command
-    @@desc = nil
-    @@format = nil
-    def desc;    @@desc    end
-    def format;  @@format  end
-    def action(arg); end
+    def self.desc; nil end
+    def self.arg_format; nil end
+    def self.action(arg); end
   end
 
   class Run < Command
-    @@desc = "Run & Post to Reddit"
-    @@format = "subreddit_name(,subreddit_name)"
+    def self.desc; "Run & Post to Reddit" end
+    def self.arg_format; "subreddit_name(,subreddit_name)" end
     def self.action(subreddits)
       RunController.post_new_videos(subreddits)
     end
   end
 
   class Update < Command
-    @@desc = "Update the DB without posting to Reddit"
-    @@format = "subreddit_name(,subreddit_name)"
+    def self.desc; "Update the DB without posting to Reddit" end
+    def self.arg_format; "subreddit_name(,subreddit_name)" end
     def self.action(subreddits)
       RunController.update_recent_videos(subreddits)
       puts "Videos for #{subreddits.join(", ")} successfully updated!"
@@ -25,16 +23,16 @@ class BarnabusController
   end
 
   class Init < Command
-    @@desc = "Make a new subreddit. Follow the directions"
-    @@format = "subreddit_name"
+    def self.desc; "Make a new subreddit. Follow the directions" end
+    def self.arg_format; "subreddit_name" end
     def self.action(subreddits)
       InitController.new_subreddit(subreddits_input.first)
     end
   end
 
   class AddChannel < Command
-    @@desc = "Add a YouTube Channel to an existing subreddit"
-    @@format = "subreddit_name"
+    def self.desc; "Add a YouTube Channel to an existing subreddit" end
+    def self.arg_format; "subreddit_name" end
     def self.action(subreddits)
       # Add a YoutubeChannel to a subreddit
       puts "To be implemented later."
@@ -42,8 +40,8 @@ class BarnabusController
   end
 
   class DeleteChannel < Command
-    @@desc = "Delete a YouTube Channel from an existing subreddit"
-    @@format = "subreddit_name"
+    def self.desc; "Delete a YouTube Channel from an existing subreddit" end
+    def self.arg_format; "subreddit_name" end
     def self.action(subreddits)
       # Delete a YoutubeChannel from a subreddit
       puts "To be implemented later."
@@ -51,8 +49,8 @@ class BarnabusController
   end
 
   class DeleteSubreddit < Command
-    @@desc = "Delete a subreddit"
-    @@format = "subreddit_name"
+    def self.desc; "Delete a subreddit" end
+    def self.arg_format; "subreddit_name" end
     def self.action(subreddits)
       # Delete a subreddit
       puts "To be implemented later."
@@ -60,8 +58,8 @@ class BarnabusController
   end
 
   class CleanVideos < Command
-    @@desc = "Still figuring this one out"
-    @@format = ""
+    def self.desc; "Still figuring this one out" end
+    def self.arg_format; "" end
     def self.action(subreddits)
       # ??
       puts "To be implemented later."
@@ -69,8 +67,8 @@ class BarnabusController
   end
 
   class Help < Command
-    @@desc = "I hope you know what this does :)"
-    @@format = ""
+    def self.desc; "I hope you know what this does :)" end
+    def self.arg_format; "" end
     def self.action(subreddits)
       HelpController.help
     end
@@ -87,12 +85,12 @@ class BarnabusController
     "--help"             => Help
     # "-h"                 => Help
   }
-  ALIASES = {
+  COMMAND_ALIASES = {
     "-h" => Help
   }
 
   def self.process_command(input, subreddits)
-    command = COMMANDS[input] || ALIASES[input]
+    command = COMMANDS[input] || COMMAND_ALIASES[input]
     if command
       command.action subreddits
     else
