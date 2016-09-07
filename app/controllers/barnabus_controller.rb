94767454b84889d7e6a7b5d4f00c47e503007202
RyanVerhey <ryan90v@gmail.com>
2016-09-06 20:33:56 -0400

    Added some named arguments

--- app/controllers/barnabus_controller.rb
+++ app/controllers/barnabus_controller.rb
@@ -8,7 +8,7 @@ class BarnabusController
   class Run < Command
     def self.desc; "Run & Post to Reddit" end
     def self.arg_format; "subreddit_name(,subreddit_name)" end
-    def self.action(subreddits)
+    def self.action(subreddits:, input:)
       RunController.post_new_videos(subreddits)
     end
   end
@@ -16,7 +16,7 @@ class BarnabusController
   class Update < Command
     def self.desc; "Update the DB without posting to Reddit" end
     def self.arg_format; "subreddit_name(,subreddit_name)" end
-    def self.action(subreddits)
+    def self.action(subreddits:, input:)
       RunController.update_recent_videos(subreddits)
       puts "Videos for #{subreddits.join(", ")} successfully updated!"
     end
@@ -25,15 +25,15 @@ class BarnabusController
   class Init < Command
     def self.desc; "Make a new subreddit. Follow the directions" end
     def self.arg_format; "subreddit_name" end
-    def self.action(subreddits)
-      InitController.new_subreddit(subreddits_input.first)
+    def self.action(subreddits:, input:)
+      InitController.new_subreddit(input.first)
     end
   end
 
   class AddChannel < Command
     def self.desc; "Add a YouTube Channel to an existing subreddit" end
     def self.arg_format; "subreddit_name" end
-    def self.action(subreddits)
+    def self.action(subreddits:, input:)
       # Add a YoutubeChannel to a subreddit
       puts "To be implemented later."
     end
@@ -42,7 +42,7 @@ class BarnabusController
   class DeleteChannel < Command
     def self.desc; "Delete a YouTube Channel from an existing subreddit" end
     def self.arg_format; "subreddit_name" end
-    def self.action(subreddits)
+    def self.action(subreddits:, input:)
       # Delete a YoutubeChannel from a subreddit
       puts "To be implemented later."
     end
@@ -51,7 +51,7 @@ class BarnabusController
   class DeleteSubreddit < Command
     def self.desc; "Delete a subreddit" end
     def self.arg_format; "subreddit_name" end
-    def self.action(subreddits)
+    def self.action(subreddits:, input:)
       # Delete a subreddit
       puts "To be implemented later."
     end
@@ -60,7 +60,7 @@ class BarnabusController
   class CleanVideos < Command
     def self.desc; "Still figuring this one out" end
     def self.arg_format; "" end
-    def self.action(subreddits)
+    def self.action(subreddits:, input:)
       # ??
       puts "To be implemented later."
     end
@@ -69,7 +69,7 @@ class BarnabusController
   class Help < Command
     def self.desc; "I hope you know what this does :)" end
     def self.arg_format; "" end
-    def self.action(subreddits)
+    def self.action(subreddits:, input:)
       HelpController.help
     end
   end
@@ -88,10 +88,10 @@ class BarnabusController
     "-h" => Help
   }
 
-  def self.process_command(input, subreddits)
-    command = COMMANDS[input] || COMMAND_ALIASES[input]
-    if command
-      command.action subreddits
+  def self.process_command(command:, input:, subreddits:)
+    command_class = COMMANDS[command] || COMMAND_ALIASES[command]
+    if command_class
+      command_class.action subreddits: subreddits, input: input
     else
       puts "That command is not recognized. Type 'use --help' for a list of commands."
     end
--- barnabus.rb
+++ barnabus.rb
@@ -4,4 +4,6 @@ input = ARGV
 subreddit_inputs = input[1].to_s.split ","
 subreddits = Subreddit.where name: subreddit_inputs
 
-BarnabusController.process_command input.first, subreddits
+BarnabusController.process_command command: input.first,
+                                   subreddits: subreddits,
+                                   input: subreddit_inputs
