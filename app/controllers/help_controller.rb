67b02942a4e5f14254057e9737302e77fcd1eae9
RyanVerhey <ryan90v@gmail.com>
2016-09-06 20:19:23 -0400

    Updating for new barnabus controller

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,7 +1,7 @@
 class HelpController
   def self.help
-    command_top_length = BarnabusController::COMMANDS.reduce(0) do |length,(command,options)|
-      command_format = "#{command} #{options[:format]}"
+    command_top_length = BarnabusController::COMMANDS.reduce(0) do |length,(command,action_class)|
+      command_format = "#{command} #{action_class.arg_format}"
       if command_format.length > length
         command_format.length
       else
@@ -10,10 +10,10 @@ class HelpController
     end
     aliases = BarnabusController::COMMAND_ALIASES.invert
 
-    BarnabusController::COMMANDS.each do |command,options|
-      command_format = "#{command} #{options[:format]}"
-      puts "#{command_format.ljust(command_top_length)} # #{options[:desc]}"
-      puts "  [#{aliases[options]}]\n" if aliases[options]
+    BarnabusController::COMMANDS.each do |command,action_class|
+      command_format = "#{command} #{action_class.arg_format}"
+      puts "#{command_format.ljust(command_top_length)} # #{action_class.desc}"
+      puts "  [#{aliases[action_class]}]\n" if aliases[action_class]
     end
   end
 end
