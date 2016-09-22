3f92980a7e078759d0a8470d7f7f734a94973bd4
RyanVerhey <ryan90v@gmail.com>
2016-09-22 17:31:07 -0400

    Added method for determining if a youtube channel exists in the db

--- app/controllers/add_channel_controller.rb
+++ app/controllers/add_channel_controller.rb
@@ -2,4 +2,8 @@ class AddChannelController
   def self.add_channel(channel_name)
 
   end
+
+  def self.youtube_channel_exists_in_db?(channel_name)
+    !!YoutubeChannel.where("name = :channel_name OR username = :channel_name", channel_name: channel_name).first
+  end
 end
