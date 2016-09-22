6695004ac2ffc4dd720ad6e8695b8ac6ec0a2d58
RyanVerhey <ryan90v@gmail.com>
2016-09-22 18:07:25 -0400

    Moved some methods around

--- app/controllers/add_channel_controller.rb
+++ app/controllers/add_channel_controller.rb
@@ -1,9 +1,21 @@
 class AddChannelController
-  def self.add_channel(channel_name)
+  def self.add_channel(subreddit, channels)
 
   end
 
+  def self.find_or_create_youtube_channel(channel_name)
+    if youtube_channel_exists_in_db? channel_name
+      fetch_youtube_channel channel_name
+    else
+      # Fetch channel info
+    end
+  end
+
   def self.youtube_channel_exists_in_db?(channel_name)
-    !!YoutubeChannel.where("name = :channel_name OR username = :channel_name", channel_name: channel_name).first
+    !!fetch_youtube_channel(channel_name)
+  end
+
+  def self.fetch_youtube_channel(channel_name)
+    YoutubeChannel.where("name = :channel_name OR username = :channel_name", channel_name: channel_name).first
   end
 end
