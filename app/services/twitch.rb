967234735f900e6b8ae9b7f79eb602d6ed902227
RyanVerhey <ryan90v@gmail.com>
2017-01-10 00:16:04 -0500

    Fetching recent videos from Twitch

--- app/services/twitch.rb
+++ app/services/twitch.rb
@@ -8,7 +8,21 @@ class Twitch  < VideoServiceBase
   URL_BASE = "https://api.twitch.tv/kraken/"
 
   def self.get_new_recent_videos(channel:, subreddit:)
-    # get new recent videos
+    assignment = ChannelAssignment.find_by(channel: channel,
+                                           subreddit: subreddit)
+    regexp = assignment.regexp
+
+    response = Twitch.get("#{URL_BASE}channels/#{channel.id}/videos?limit=10&broadcast_type=archive,highlight,upload", REQUEST_OPTIONS)
+
+    video_data = response['videos']
+
+    videos = video_data.map do |video|
+      title = video['title']
+      desc = video['description'].to_s
+      if regexp.match(title) || regexp.match(desc)
+        video_object_from_twitch_video_data(video)
+      end
+    end
   end
 
   def self.initialize_channel(name)
@@ -30,6 +44,19 @@ class Twitch  < VideoServiceBase
     end
 
     channel
+  end
+
+  def self.video_object_from_twitch_video_data(video)
+    vid = TwitchVideo.find_or_initialize_by id: video["_id"]
+
+    vid.attributes = {
+      title: video['title'],
+      author: video['channel']['display_name'],
+      description: video['description'].to_s,
+      published_at: video['published_at']
+    }
+
+    vid
   end
 
   def self.channel
