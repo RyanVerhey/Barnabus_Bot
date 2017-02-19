9cf88e63fd92951cba9efc24d808bd2ada940d67
RyanVerhey <ryan90v@gmail.com>
2017-02-19 13:19:02 -0500

    Making sure reddit_posts for video are deleted when the video is deleted too

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,13 @@ class Video < ActiveRecord::Base
   self.primary_key = "id"
 
   belongs_to :channel
+  has_many :reddit_posts, foreign_key: :video_id
+
+  before_destroy do
+    reddit_posts.each do |post|
+      post.destroy
+    end
+  end
 
   def self.without_associated_channel
     all.select { |video| video.channels.empty? }
