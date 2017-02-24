c49b7b84cd29957f4447df3c6c3506d21a549941
RyanVerhey <ryan90v@gmail.com>
2017-02-24 18:44:17 -0500

    Adding posts and videos to Subreddit

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -2,6 +2,8 @@ class Subreddit < ActiveRecord::Base
   belongs_to :account, class_name: "RedditAccount", foreign_key: "reddit_account_id"
   has_many :channel_assignments
   has_many :channels, through: :channel_assignments
+  has_many :posts, class_name: "RedditPost"
+  has_many :videos, through: :posts
   accepts_nested_attributes_for :channel_assignments
 
   def to_s
