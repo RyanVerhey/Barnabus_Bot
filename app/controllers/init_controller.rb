056d8d41d50c52a51ceeedf4df4657dd2401aa15
RyanVerhey <ryan90v@gmail.com>
2017-02-24 18:45:14 -0500

    Rewrote init controller for file-based adding

--- app/controllers/init_controller.rb
+++ app/controllers/init_controller.rb
@@ -1,7 +1,75 @@
 class InitController
 
-  def self.new_subreddit(subreddit, filename)
+  def initialize(data)
+    @data = data
+  end
 
+  def self.new_subreddit(subreddit, filename)
+    data = YAML.load File.open "#{ APP_DIR }/#{filename.first}"
+    self.new(data).send(:add_subreddit)
+  end
+
+  private
+
+  def add_subreddit
+    @subreddit = instantiate_subreddit
+    @reddit_account = instantiate_or_find_reddit_account
+    @channels = instantiate_or_find_channels
+
+    save_all
+  end
+
+  def instantiate_subreddit
+    subreddit_data = @data['subreddit']
+    subreddit = Subreddit.find_or_initialize_by(name: subreddit_data['name'])
+    check_for_existing_subreddit subreddit
+    subreddit
+  end
+
+  def check_for_existing_subreddit(subreddit)
+    unless subreddit.new_record?
+      puts "Subreddit #{ subreddit } already in database"
+      exit
+    end
+  end
+
+  def instantiate_or_find_reddit_account
+    account_data = @data['subreddit']['reddit_account']
+    account = RedditAccount.find_or_initialize_by(username: account_data['name'])
+    if account.new_record?
+      account.password_var = account_data['password_var']
+    end
+    account
+  end
+
+  def instantiate_or_find_channels
+    channel_data = @data['subreddit']['channels']
+    channels = channel_data.map do |c|
+      channel_class = SubredditService.channel_for_channel_type(c['type'])
+      channel = channel_class.find_or_initialize_by(name: c['name'])
+      channel.regexp = Regexp.new(c['regexp'], Regexp::IGNORECASE)
+      yt_channel_data = YouTube.youtube_channel_data username: c['username'], id: c['id']
+      if channel.new_record?
+        channel.name = yt_channel_data[:name]
+        channel.id = yt_channel_data[:id]
+        channel.username = c['username']
+      end
+      channel
+    end
+    channels
+  end
+
+  def save_all
+    @reddit_account.save if @reddit_account.new_record?
+    @subreddit.account = @reddit_account
+    @subreddit.save
+    @channels.each(&:save)
+
+    @channels.each do |channel|
+      ChannelAssignment.create(channel: channel, subreddit: @subreddit, regexp: channel.regexp)
+    end
+
+    RunController.update_recent_videos(@subreddit)
   end
 
 end
