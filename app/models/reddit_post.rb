93e538631051238529c7e583868bf96ff79c5467
RyanVerhey <ryan90v@gmail.com>
2017-03-11 10:34:08 -0500

    added message

--- app/models/reddit_post.rb
+++ app/models/reddit_post.rb
@@ -2,5 +2,5 @@ class RedditPost < ActiveRecord::Base
   belongs_to :video
   belongs_to :subreddit
 
-  validates :video_id, uniqueness: { scope: :subreddit_id }
+  validates :video_id, uniqueness: { scope: :subreddit_id, message: "Videos can only be posted once to a subreddit" }
 end
